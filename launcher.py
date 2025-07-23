71aa58be0eb334224640df0836e1b20842c0d89a
RyeHands <66764866+RyeHands@users.noreply.github.com>
2025-07-23 14:52:07 -0700

    Update launcher.py
    fixing more update bugs

--- launcher.py
+++ launcher.py
@@ -7,6 +7,11 @@ def run_updater_if_present():
         subprocess.run(["python", "auto_updater.py"])
         print("[Launcher] Updater finished.")
 
+    # If an update is in progress, don't run the app
+    if os.path.exists("update_in_progress.flag"):
+        print("[Launcher] Update is in progress. Exiting launcher.")
+        sys.exit(0)
+
 def launch_main_app():
     print("[Launcher] Launching app...")
     subprocess.run(["python", "web.py"])
